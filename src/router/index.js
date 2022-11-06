32c319e5d0b8fca56a3ff5151653bba91a5ba360
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-11-06 19:08:09 +0700

    feat: created why mac page and tech specs page

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,19 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomeView from "../views/HomeView.vue";
 
-const routes = [{ path: "/", name: "Home", component: HomeView }];
+const routes = [
+  { path: "/", name: "Home", component: HomeView },
+  {
+    path: "/whymac",
+    name: "Why Mac",
+    component: import("../views/WhyMacView.vue"),
+  },
+  {
+    path: "/techspecs",
+    name: "Tech Specs",
+    component: import("../views/TechSpecView.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
---
+++ src/views/TechSpecView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>this is tech spec view</p>
+  </div>
+</template>
