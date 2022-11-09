3907f8839b95cf3982694981e8764596d7646708
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-11-10 04:38:25 +0700

    chore: responsive macbook pro section for mobile devices, sm devices, md devices, lg devices, xl devices and 2xl devices

--- src/components/techSpecs/MacBookProSection.vue
+++ src/components/techSpecs/MacBookProSection.vue
@@ -1,12 +1,18 @@
 <template>
+  <hr
+    class="md:hidden"
+    style="height: 2px; border-width: 0; background-color: #1d1d1f"
+  />
   <div class="flex justify-center">
-    <div class="grid mt-20">
+    <div class="grid place-content-center place-items-center mt-20">
       <p
-        class="font-semibold text-[#f5f5f7] font-['SF_Pro_Display_Light'] xl:text-5xl"
+        class="font-semibold text-[#f5f5f7] font-['SF_Pro_Display_Light'] text-[32px] md:text-[40px] xl:text-5xl"
       >
         MacBook Pro
       </p>
-      <div class="xl:flex xl:gap-5 mt-10">
+      <div
+        class="grid place-items-center md:flex md:gap-4 xl:gap-5 mt-5 xl:mt-10"
+      >
         <a
           href="https://www.apple.com/us/shop/goto/buy_mac/macbook_pro_13"
           target="_blank"
@@ -20,8 +26,22 @@
           >Compare all Mac models
         </a>
       </div>
+      <img
+        src="../../assets/mbp_shop_large.png"
+        alt=""
+        class="hidden md:block mt-14"
+      />
+      <img
+        src="../../assets/mbp_shop_small.png"
+        class="h-40 mt-10 md:hidden"
+        alt=""
+      />
     </div>
   </div>
+  <hr
+    class="mt-14"
+    style="height: 2px; border-width: 0; background-color: #1d1d1f"
+  />
 </template>
 
 <style scoped>
