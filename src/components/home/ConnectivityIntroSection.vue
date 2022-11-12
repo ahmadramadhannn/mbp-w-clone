f3813451625f4ef532045941512dc437582d593d
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-11-12 15:55:20 +0700

    fix: connectivity section not responsive

--- src/components/home/ConnectivityIntroSection.vue
+++ src/components/home/ConnectivityIntroSection.vue
@@ -1,21 +1,23 @@
 <template>
-  <div
-    class="mt-24 lg:mt-32 2xl:mt-40 grid place-content-center place-items-center overflow-hidden"
-  >
-    <div class="grid w-10/12 sm:w-[400px] md:w-[680px] xl:w-[940px]">
-      <p
-        class="font-['SF_Pro_Medium'] leading-[50px] xl:leading-[80px] font-bold text-[#a1a1a6] text-5xl md:text-[64px] xl:text-[80px]"
-      >
-        Connectivity
-      </p>
-      <p
-        class="text-[#f5f5f7] xl:w-[700px] font-bold text-5xl md:text-[64px] xl:text-[80px] font-['SF_Pro_Medium']"
-      >
-        Make connections. Faster than ever.
-      </p>
-    </div>
+  <div class="md:h-[32em] lg:h-[34em] xl:h-[45em] overflow-hidden">
     <div
-      class="mt-14 md:mt-20 xl:mt-24 w-full sm:w-[200%] md:w-[150%] xl:w-[1340px] 2xl:w-[350%] h-[150px] md:h-[350px] xl:h-[500px] bg-[url('https://res.cloudinary.com/dk7urhntn/image/upload/v1668238554/macbook/connectivity__e3efjadx4suq_large_biawte.jpg')] bg-cover bg-no-repeat bg-center"
-    ></div>
+      class="mt-24 lg:mt-32 xl:mt-40 grid place-content-center place-items-center"
+    >
+      <div class="grid w-10/12 sm:w-[400px] md:w-[680px] xl:w-[940px]">
+        <p
+          class="font-['SF_Pro_Medium'] leading-[50px] xl:leading-[80px] font-bold text-[#a1a1a6] text-5xl md:text-[64px] xl:text-[80px]"
+        >
+          Connectivity
+        </p>
+        <p
+          class="text-[#f5f5f7] xl:w-[700px] font-bold text-5xl md:text-[64px] xl:text-[80px] font-['SF_Pro_Medium']"
+        >
+          Make connections. Faster than ever.
+        </p>
+      </div>
+      <div
+        class="mt-14 md:mt-20 xl:mt-24 w-full sm:w-[200%] md:w-[150%] xl:w-[1340px] 2xl:w-[350%] h-[150px] md:h-[350px] xl:h-[500px] bg-[url('https://res.cloudinary.com/dk7urhntn/image/upload/v1668238554/macbook/connectivity__e3efjadx4suq_large_biawte.jpg')] bg-cover bg-no-repeat bg-center"
+      ></div>
+    </div>
   </div>
 </template>
